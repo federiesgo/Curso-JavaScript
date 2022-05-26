3c663a6e051db6c9260fe225d0c1cc2a7c53fc92
Federico Riesgo <fede_riesgo@hotmail.com>
2022-05-26 19:42:40 -0300

    Desafio incorporando librerias

---
+++ Desafio Incorporando Librerias/js/footer.js
@@ -0,0 +1,50 @@
+var footer1 = document.getElementById('footer1');
+footer1.innerHTML += `<section class="bg-dark">
+<div class="container text-center text-md-start mt-5">
+    <div class="row mt-3">
+        <div class="col-12 col-xxl-3 col-xl-3 col-sm-12 col-md-3 col-lg-3 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">
+                Sobre Nosotros
+            </h6>
+            <p>
+                Somos un espacio dedicado especialmente al cuidado de tu salud y belleza. Te ofrecemos una atencion personalizada y profesional. ¡Solicita tu turno!
+            </p>
+        </div>
+        <div class="col-12 col-xxl-3 col-xl-3 col-sm-12 col-md-3 col-lg-3 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">
+                Servicios
+            </h6>
+            <p>
+                <a href="html/servicios.html" class="text-reset">Pestañas</a>
+            </p>
+            <p>
+                <a href="html/servicios.html" class="text-reset">Esculpidas</a>
+            </p>
+            <p>
+                <a href="html/servicios.html" class="text-reset">Masajes</a>
+            </p>
+        </div>
+        <div class="col-12 col-xxl-3 col-xl-3 col-sm-12 col-md-3 col-lg-3 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">
+                Contacto
+            </h6>
+            <p> Buenos Aires, Bella Vista</p>
+            <p>
+                empar-spa@gmail.com
+            </p>
+            <p> +54 9 1153440036</p>
+        </div>
+        <div class="col-12 col-xxl-3 col-xl-3 col-sm-12 col-md-3 col-lg-3 mx-auto mb-4">
+            <h6 class="text-uppercase fw-bold mb-4">
+                RRSS
+            </h6>
+            <a href="https://www.instagram.com/empar.ok/" target="blank_"><img src="img/instagram.png" height="20px" width="20px" alt="Logo de Instagram"></a>
+            <a href="https://www.facebook.com/EMPAR-Dise%C3%B1o-y-Belleza-1511007562542622" target="blank_"><img src="img/facebook.png" height="20px" width="20px" alt="Logo de Facebook"></a>
+        </div>
+    </div>
+</div>
+</section>
+<div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+© 2022 Todos los derechos reservados:
+<a class="text-reset fw-bold">Empar Spa</a>
+</div>`
