c50655c4453de5fe031d048fa9e27019a822f050
Federico Riesgo <fede_riesgo@hotmail.com>
2022-06-04 21:38:07 -0300

    navbar terminado para dispositivos grandes

--- Prueba Proyecto final/js/navbar.js
+++ Prueba Proyecto final/js/navbar.js
@@ -4,3 +4,28 @@ $('.menu-toggle').click(function() {
     $(this).toggleClass('open');
 
 })
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        document.getElementById("header").style.background = "antiquewhite";
+        document.getElementById("logoAnchor").style.color = "black"
+        let navA = document.getElementsByClassName("navA");
+        let navAElem;
+        modificandoElemsDeClass(navAElem, navA, "black")
+
+    } else {
+        document.getElementById("header").style.background = "transparent";
+        document.getElementById("logoAnchor").style.color = "white"
+        let navA = document.getElementsByClassName("navA");
+        let navAElem;
+        modificandoElemsDeClass(navAElem, navA, "white")
+    }
+}
+
+function modificandoElemsDeClass(elem, clase, colorElegido) {
+    for (elem = 0; elem < clase.length; elem++) {
+        clase[elem].style.color = colorElegido;
+    }
+}
